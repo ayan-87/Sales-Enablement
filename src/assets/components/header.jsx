92d13da676eb4a065576c2a39b6f10afe808d028
ayan-87 <ayan.cottonian87@gmail.com>
2024-03-21 13:20:26 +0530

    first commit

---
+++ src/assets/components/header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './header.css'
+import logo from '../images/Yara_logo.svg.png'
+
+function Header(){
+    return (
+        <header>
+            <ul>
+                <li><img class = 'logo'src={logo} alt="logo" /></li>
+                <li><p> sales Enablement</p></li>
+            </ul>
+            <ul>
+                <li> <a href="#">about </a> </li>
+                <li><a href="">bell</a></li>
+                <li><a href="">account</a></li>
+            </ul>
+        </header>
+    )
+}
+export default Header;
